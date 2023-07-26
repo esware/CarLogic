0667f25eaa8e835a3dccb0830fd3a16f9ff382d0
Serhat <48649947+esware@users.noreply.github.com>
2023-07-26 23:03:03 +0300

    created levelmanager

---
+++ Assets/Dev/ScriptableObjects/LevelData.cs
@@ -0,0 +1,12 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ScriptableObjects.Scripts
+{
+    [CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
+    public class LevelData : ScriptableObject
+    {
+        [Header("Level")] [SerializeField] public GameObject levelObject;
+
+    }
+}
---
+++ Assets/Dev/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using ScriptableObjects.Scripts;
+using UnityEngine;
+
+ namespace Managers
+{
+    public struct GameEvents
+    {
+        public static Action WinEvent;
+        public static Action LoseEvent;
+
+        public static void DestroyEvents()
+        {
+            WinEvent = null;
+            LoseEvent = null;
+        }
+        
+    }
+    
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private bool transitionWithPrefab;
+        [SerializeField] private GameData gameData;
+
+        [HideInInspector] public LevelData levelData;
+        
+        private const int LevelResetIndex = 1;
+        
+        private int _currentLevel;
+
+        private int _levelIndex;
+        
+
+        private void Awake()
+        {
+            LoadLevel();
+        }
+
+        private void LoadLevel()
+        {
+            InitLevelData();
+
+            if (transitionWithPrefab)
+            {
+                InstantiateLevel();
+            }
+        }
+
+        private void InitLevelData()
+        {
+            _currentLevel = PlayerPrefs.GetInt("PlayerLevel");
+
+            var lastLevelIndex = gameData.LastLevelIndex;
+            _levelIndex = lastLevelIndex == gameData.levelsDataArray.Length - 1 ? LevelResetIndex : lastLevelIndex + 1;
+            levelData = gameData.levelsDataArray[_levelIndex];
+            
+#if UNITY_EDITOR
+            Debug.Log($"Current Level: {_currentLevel.ToString()}");
+#endif
+        }
+        
+        private static void IncreasePlayerPrefLevel()
+        {
+            PlayerPrefs.SetInt("PlayerLevel", PlayerPrefs.GetInt("PlayerLevel") + 1);
+        }
+        private void InstantiateLevel()
+        {
+            Instantiate(gameData.levelsDataArray[_currentLevel % gameData.levelsDataArray.Length].levelObject);
+
+            levelData = gameData.levelsDataArray[_currentLevel % gameData.levelsDataArray.Length];
+        }
+        private void OnDestroy()
+        {
+            GameEvents.DestroyEvents();
+        }
+    }
+}
